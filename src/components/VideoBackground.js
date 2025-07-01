3bf46c3ef3306d9e7afb73aaea13eb216078b133
Mridul Dutt <mirdul12dutt@gmail.com>
2025-07-01 18:33:17 +0530

    changes

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,40 +1,31 @@
 import React from "react";
 import Loader from "./Loader";
-import {useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import useTrailerMovie from "../customHooks/useTrailerMovie";
 
-
 const VideoBackground = (props) => {
- 
-  const trailerVideo=useSelector(store=>store?.movies?.trailerVideo);
+  const trailerVideo = useSelector((store) => store?.movies?.trailerVideo);
   useTrailerMovie(props);
-    
-    
+
   return (
-      <>
+    <>
       {trailerVideo?.key ? (
-        <div className="relative w-[100vw] h-[100vh] overflow-hidden">
-          {/* Fullscreen Video as Background */}
+        <div className="w-[100vw] h-[100vh]">
           <iframe
-            className="absolute top-0 left-0 w-full h-full object-cover pointer-events-none z-0"
-            src={`https://www.youtube.com/embed/${trailerVideo?.key}?controls=0&loop=1&autoplay=1&mute=1&playlist=${trailerVideo?.key}`}
+            className="w-screen top-0 left-0 w-full h-full object-cover pointer-events-none z-0 aspect-video"
+            src={`https://www.youtube.com/embed/${trailerVideo?.key}?controls=0&loop&autoplay=1&mute=1&playlist=${trailerVideo?.key}`}
             title="YouTube video player"
-            frameBorder="0"
+            autoPlay
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
           ></iframe>
+
+
         </div>
       ) : (
         <Loader />
       )}
     </>
-  )
+  );
 };
 
 export default VideoBackground;
-
-
-
-
-
-
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
-const VideoTitle = ({title,overview}) => {
-
-    
+const VideoTitle = (props) => {
+  
+  // eslint-disable-next-line react/prop-types
+  const {title,overview} = props;
+// console.log(overview);
   return (
-    <div className='w-3/12 p-2 mx-6 text-white absolute my-[25%] sm:my-[21%] md:my-[17%]'>
+    <div className='lg:w-3/12 sm:w-1/2 p-2 mx-6 text-white absolute my-[25%] sm:my-[21%] md:my-[17%]'>
      <div className='text-lg sm:text-xl md:text-3xl p-2 m-2 font-bold'>
         {title}
      </div>
-     <div className='hidden text-gray-300 lg:block p-2 m-2'>
+     <div className=' text-gray-300 lg:block p-2 m-2 '>
         {overview}
+     </div>
+
+     <div className='flex md:mx-[10%]'>
+       <button className='bg-white text-lg text-black mx-[10%] md:mx-0 lg:px-[12%]  lg:py-[4%] py-[2%] px-[2%]  md:px-[5%] md:py-[0%] rounded-lg   m-2  flex justify-center items-center hover:bg-opacity-80'> ▶ PLAY</button>
+       <button className='bg-white text-lg text-black lg:py-[4%] lg:px-[10%]   md:px-[5%] sm:px-[5%] md:py-[2%] rounded-lg  m-2 flex items-center justify-center opacity-80 hover:bg-opacity-90'>More Info</button>
      </div>
     </div>
   )
